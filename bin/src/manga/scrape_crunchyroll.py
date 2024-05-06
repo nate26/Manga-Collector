43fdd33578d6eaea1899188cebe76fdac33b3ef2
nate26 <42745307+nate26@users.noreply.github.com>
2024-05-05 22:04:42 -0400

    some fixes caught

--- bin/src/manga/scrape_crunchyroll.py
+++ bin/src/manga/scrape_crunchyroll.py
@@ -22,17 +22,23 @@ import requests
 from bs4 import BeautifulSoup
 
 # TO DO:
+# - fix release date!!!!!
+# - fix series vol sort to order by number not string :P
 # - add more error handling
 # - add more shops
 # - get better additional images / better way to get images/descriptions
 # - fix amazon stock status
-# - find a way to get age ratings / adult tag
+# - find a way to get age ratings / adult tag -> Amazon has this
+# - fix b&n query and check if b&n ever has a sale?
 
 RUN_SCRAPER = True
 SCRAPE_ALL_PAGES = True
 
+START = 600
+
 QUERY_ISBN_DB = True
 QUERY_CR_FOR_DETAILS = False
+QUERY_BARNS_AND_NOBLE = False
 
 # only used if requires a full refresh of all data
 FORCE_CR_FOR_DETAILS = False
@@ -76,6 +82,7 @@ def save_file(file_path, data):
 
 def save_all_files(volumes_provided, series_provided, shop_provided):
     '''Saves all the given data structures to their respective files.'''
+    logger.info('Saving all files...')
     save_file('./volumes.json', volumes_provided)
     save_file('./series.json', series_provided)
     save_file('./shop.json', shop_provided)
@@ -91,6 +98,11 @@ def parse_volume(display_name: str, category: str):
         return re.search(
             r'\d+\.?\-?\d*',
             re.search(r' [()]?Vol[a-z]?[()]? \d+\.?\-?\d*', display_name).group(0)
+        ).group(0)
+    if re.search(r' [()]?Vol.[a-z]?[()]? \d+\.?\d*', display_name):
+        return re.search(
+            r'\d+\.?\-?\d*',
+            re.search(r' [()]?Vol.[a-z]?[()]? \d+\.?\-?\d*', display_name).group(0)
         ).group(0)
     if re.search(r' [()]?Graphic Novel[a-z]?[()]? \d+\.?\-?\d*', display_name):
         return re.search(
@@ -114,11 +126,11 @@ def get_series_by_id(series_id: str):
     logger.info('Getting series details for %s...', series_id)
     try:
         if series_id in series_cache:
-            series_resp = series_cache[series_id]
-            logger.info('Pulled series from local cache: %s', json.dumps(series_resp))
+            parsed_series_data = series_cache[series_id]
+            logger.info('Pulled series from local cache: %s', json.dumps(parsed_series_data))
         else:
             series_resp = requests.get('https://api.mangaupdates.com/v1/series/' + series_id,
-                                    timeout=5).json()
+                                    timeout=30).json()
             parsed_series_data = {
                 'series_id': str(series_resp['series_id']),
                 'title': series_resp['title'],
@@ -164,11 +176,14 @@ def search_series(series_name: str, category: str, volume_name: str):
         'stype': 'title'
     }
     try:
+        if series_name is None:
+            raise AttributeError('series name is None')
+
         series_resp = requests.post('https://api.mangaupdates.com/v1/series/search',
-                                    search_data, timeout=5).json()
+                                    search_data, timeout=30).json()
         # only used if no exact match is found
         closest_series_match = None
-        for series in [series_resp['results'][0]]: # fix later for match logic
+        for series in [series_resp['results'][0]]:
             series_details = get_series_by_id(str(series['record']['series_id']))
             logger.info('Checking series: %s', json.dumps(series_details))
 
@@ -212,7 +227,7 @@ def search_series(series_name: str, category: str, volume_name: str):
                         closest_series_match['series_match_confidence'],
                         json.dumps(closest_series_match))
             return closest_series_match
-    except requests.exceptions.RequestException as e_search:
+    except (requests.exceptions.RequestException, IndexError, AttributeError) as e_search:
         logger.error(e_search)
         logger.error('Could not get series ID for "%s"... ending process', series_name)
 
@@ -232,7 +247,8 @@ def get_isbn_details(soup_isbn_data):
         'publisher': None,
         'format': None,
         'pages': None,
-        'authors': None
+        'authors': None,
+        'isbn_10': None
     }
     try:
         text_tags = soup_isbn_data.find('div', {'class': 'book-info'}).find_all('dt')
@@ -262,6 +278,9 @@ def get_isbn_details(soup_isbn_data):
             if 'Authors:' in tag.text and details['authors'] is None:
                 details['authors'] = tag.text.split(': ')[1].strip()
                 logger.info('Found authors: %s', details['authors'])
+            if 'ISBN 10:' in tag.text and details['isbn_10'] is None:
+                details['isbn_10'] = tag.text.split(': ')[1].strip()
+                logger.info('Found ISBN 10: %s', details['isbn_10'])
             if None not in details.values():
                 break
         if None in details.values():
@@ -270,7 +289,7 @@ def get_isbn_details(soup_isbn_data):
         logger.warning('Could not find book details from isbn... ending process because %s', err)
     return details
 
-def get_shop_data(soup_isbn_data):
+def get_shop_data(soup_isbn_data, isbn_10: str):
     '''Gets all the valid shop details from the given isbn soup object.'''
     shops = []
     try:
@@ -284,7 +303,7 @@ def get_shop_data(soup_isbn_data):
                 shops.append({
                     'store': 'Amazon',
                     'condition': store.find('td', {'class': 'condition'}).attrs['data-condition'],
-                    'url': store.find('td', {'class': 'link'}).text.strip(),
+                    'url': 'https://www.amazon.com/dp/' + isbn_10,
                     'store_price': float(store.find('td', {'class': 'total'})
                         .text.strip().replace('$', '')),
                     'stock_status': None,
@@ -302,13 +321,46 @@ def get_shop_data(soup_isbn_data):
 def isbn_search(isbn: str):
     '''Searches for the given ISBN on the ISBN search website and returns the results.'''
     soup_isbn_data = BeautifulSoup(
-        requests.get('https://www.campusbooks.com/search/' + isbn + '?buysellrent=buy', timeout=5)
+        requests.get('https://www.campusbooks.com/search/' + isbn + '?buysellrent=buy', timeout=30)
             .text,
         'html.parser'
     )
+    isbn_details = get_isbn_details(soup_isbn_data)
     return {
-        'details': get_isbn_details(soup_isbn_data),
-        'shops': get_shop_data(soup_isbn_data)
+        'details': isbn_details,
+        'shops': get_shop_data(soup_isbn_data, isbn_details['isbn_10'])
+    }
+
+def get_barnes_and_noble_data(isbn: str, vol_shop_data: list):
+    '''Gets the Barnes & Noble data for the given ISBN.'''
+    url = 'https://barnesandnoble.com/w/?ean=' + isbn
+    logger.info('Getting Barnes & Noble data for %s...', isbn)
+    soup_bn_data = BeautifulSoup(
+        requests.get(url, timeout=30).text,
+        'html.parser'
+    )
+    formats = soup_bn_data.find_all('div', {'class': 'pdp-commerce-format'})
+    valid_format = [format for format in formats if 'Paperback' in format.text]
+    store_price = float(valid_format[0].find('div', {'class': 'format-price'})
+                        .text.strip().replace('$', '')) \
+                            if len(valid_format) > 0 else None
+    stock_status = soup_bn_data.find('div', {'class': 'purchase-add-to-cart'}) \
+        .find('div', {'class': 'add-to-cart-button'}).attrs['value']
+    existing_bn_data = list(filter(lambda x: x['store'] == 'Barnes & Noble', vol_shop_data))
+    is_new_entry = len(existing_bn_data) == 0
+    existing_stock_status = existing_bn_data[0]['stock_status'] \
+        if not is_new_entry else None
+    return {
+        'store': 'Barnes & Noble',
+        'condition': 'New',
+        'url': url,
+        'store_price': store_price,
+        'stock_status': stock_status,
+        'last_stock_update': str(datetime.now()) \
+            if is_new_entry and stock_status == existing_stock_status \
+                else existing_bn_data[0]['last_stock_update'],
+        'coupon': '',
+        'is_on_sale': False
     }
 
 def scrape_page(soup, all_volumes, all_series, all_shop):
@@ -343,11 +395,12 @@ def scrape_page(soup, all_volumes, all_series, all_shop):
             isbn_results = { 'details': {}, 'shops': [] }
             logger.info('Volume exists, ISBN search skipped...')
 
-        retail_price = item \
-            .find('div', {'class': 'price'}) \
-                .find('span', {'class': 'strike-through'}) \
-                    .find('span', {'class': 'value'}) \
-                        .attrs['content']
+        retail_price = max([
+            price.attrs['content']
+            for price in
+            item.find('div', {'class': 'price'}) \
+                .find_all('span', {'class': 'value'})
+        ])
         cover_image = item.find('img', {'class': 'tile-image'}).attrs['src']
         category_id = cr_attr['categoryID'] if 'categoryID' in cr_attr else None
 
@@ -357,8 +410,8 @@ def scrape_page(soup, all_volumes, all_series, all_shop):
         stock_status = cr_attr['Inventory_Status']
         # update if stock status is different or new volume
         last_stock_update = str(datetime.now()) \
-            if is_new_volume or stock_status != all_volumes[isbn]['stock_status'] \
-                else all_volumes[isbn]['last_stock_update']
+            if is_new_volume or stock_status != all_shop[isbn]['shops'][0]['stock_status'] \
+                else all_shop[isbn]['shops'][0]['last_stock_update']
         product = {
             'isbn': isbn,
             'retail_price': float(retail_price),
@@ -377,6 +430,16 @@ def scrape_page(soup, all_volumes, all_series, all_shop):
                 *isbn_results['shops']
             ]
         }
+        # get barnes and noble data
+        if QUERY_BARNS_AND_NOBLE:
+            try:
+                bn_data = get_barnes_and_noble_data(isbn,
+                    [] if is_new_volume else all_shop[isbn]['shops'])
+                logger.info('Barnes & Noble data: %s', json.dumps(bn_data))
+                product['shops'].append(bn_data)
+            except (requests.exceptions.RequestException, IndexError) as e:
+                logger.error(e)
+                logger.error('Could not get Barnes & Noble data for %s... ending process', isbn)
         all_shop[isbn] = product
         logger.info('All shop details added: %s', json.dumps(product))
 
@@ -422,7 +485,7 @@ def scrape_page(soup, all_volumes, all_series, all_shop):
             if (QUERY_CR_FOR_DETAILS and not is_new_volume) or FORCE_CR_FOR_DETAILS:
                 logger.info('Scraping CR page for description and more cover images: %s', isbn)
                 soup_volume = BeautifulSoup(
-                    requests.get(volume['url'], timeout=5).text,
+                    requests.get(volume['url'], timeout=30).text,
                     'html.parser'
                 )
                 # get the description
@@ -477,7 +540,10 @@ def scrape_page(soup, all_volumes, all_series, all_shop):
                             *all_series[series_id]['volumes'],
                             series_volume
                         ],
-                        key = lambda x: (x['category'], x['volume'])
+                        key = lambda x: (
+                            x['category'],
+                            '-1' if x['volume'] is None else x['volume']
+                        )
                     )
                     all_series[series_id]['volumes'] = series_volumes
                     logger.info('Series exists, added volume: %s to %s', isbn,
@@ -492,7 +558,8 @@ def scrape_page(soup, all_volumes, all_series, all_shop):
                     logger.info('Series details forcefully updated: %s',
                                 json.dumps(all_series[series_id]))
 
-    save_all_files(all_volumes, all_series, all_shop)
+        # save each volume to file
+        save_all_files(all_volumes, all_series, all_shop)
     return all_volumes, all_series, all_shop
 
 if RUN_SCRAPER:
@@ -504,9 +571,9 @@ if RUN_SCRAPER:
     CATEGORY_QUERY = '&prefn1=subcategory&prefv1=Novels|Manhwa|Manhua|Light%20Novels|Manga'
     PAGE_URL = PAGE_BASE_URL + CATEGORY_QUERY
 
-    logger.info('Calling: %s&start=0&sz=100', PAGE_URL)
+    logger.info('Calling: %s&start=%s&sz=100', PAGE_URL, START)
     first_soup = BeautifulSoup(
-        requests.get(PAGE_URL + '&start=0&sz=100', timeout=5).text,
+        requests.get(PAGE_URL + f'&start={START}&sz=100', timeout=30).text,
         'html.parser'
     )
     total_count = int(first_soup.find('div', {'class': 'pagination-text'}).attrs['data-totalcount'])
@@ -516,18 +583,16 @@ if RUN_SCRAPER:
     volumes_new, series_new, shop_new \
         = scrape_page(first_soup, volumes_data, series_data, shop_data)
 
-    START = 100
-
     if SCRAPE_ALL_PAGES:
         for i in range(1, total_pages):
+            START += 100
             logger.info('Calling: %s&start=%s&sz=100', PAGE_URL, START)
             next_soup = BeautifulSoup(
-                requests.get(PAGE_URL + f'&start={START}&sz=100', timeout=5).text,
+                requests.get(PAGE_URL + f'&start={START}&sz=100', timeout=30).text,
                 'html.parser'
             )
             volumes_new, series_new, shop_new \
                 = scrape_page(next_soup, volumes_new, series_new, shop_new)
-            START += 100
 
 # print(calculate_confidence(
     # 'Re:ZERO',
