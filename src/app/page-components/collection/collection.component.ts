41126372c803987e0f5756e02c84921a4e9d113f
nate26 <42745307+nate26@users.noreply.github.com>
2024-05-28 23:18:38 -0400

    some editability

--- src/app/page-components/collection/collection.component.ts
+++ src/app/page-components/collection/collection.component.ts
@@ -1,6 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CollectionDataService } from '../../services/data/collection-data.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
+import { IVolume } from '../../interfaces/iVolume.interface';
+
+interface TextEvent extends Event {
+    target: EventTarget & { value: string };
+}
 
 @Component({
     selector: 'app-collection',
@@ -11,8 +17,43 @@ import { CollectionDataService } from '../../services/data/collection-data.servi
 })
 export class CollectionComponent {
 
-    volumes$ = this.collectionDataService.collectionVolumes$;
+    volumes = signal<IVolume[]>([]);
 
-    constructor(private collectionDataService: CollectionDataService) { }
+    editing = signal(false);
+    saveBatch = signal<{ index: number; field: string; value: Event; }[]>([]);
+
+    displayVolumes = computed(() => {
+        const vols = this.volumes();
+        if (!vols) return [];
+        const saveBatchState = this.saveBatch();
+        return vols.map((volume, idx) => {
+            const saveState = saveBatchState.find(s => s.index === idx);
+            if (saveState) {
+                const eventVal = (<TextEvent>saveState.value).target.value;
+                const newVal = saveState.field === 'tags' ? eventVal.split(',') : (saveState.field === 'cost' ? parseFloat(eventVal) : eventVal);
+                return { ...volume, user_collection_data: [{ ...volume.user_collection_data[0], [saveState.field]: newVal }] };
+            }
+            return volume;
+        });
+    });
+
+    constructor(private collectionDataService: CollectionDataService) {
+        this.collectionDataService.collectionVolumes$.pipe(
+            takeUntilDestroyed()
+        ).subscribe(volumes => {
+            this.volumes.set(volumes);
+        });
+    }
+
+    save(index: number, field: string, value: Event) {
+        this.saveBatch.update(batch => {
+            if (batch.some(b => b.index === index && b.field === field)) {
+                // update existing if was previously saved
+                return batch.map(b => b.index === index && b.field === field ? { ...b, value } : b);
+            }
+            // add save state
+            return [...batch, { index, field, value }]
+        })
+    }
 
 }
