f03236ac6d8de00ae9b3a16325ca083dd4bca9fa
nate26 <42745307+nate26@users.noreply.github.com>
2024-07-04 19:58:49 -0400

    improved styling of form

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -5,7 +5,7 @@ import { UserService } from '../../services/data/user.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Router } from '@angular/router';
 import { HttpErrorResponse } from '@angular/common/http';
-import { map, take } from 'rxjs';
+import { debounceTime, map, take } from 'rxjs';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 export const LOGIN_PATH_CONTEXT = {
@@ -42,7 +42,7 @@ export class LoginComponent {
         password: new FormControl('', [
             Validators.required,
             Validators.pattern(
-                '(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!#^~%*?&,.<>"\'\\;:{\\}\\[\\]\\|\\+\\-\\=\\_\\)\\(\\)\\`\\/\\\\\\]])[A-Za-z0-9d$@].{7,}'
+                '(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!#^~%*?&,.<>"\'\\;:{\\}\\[\\]\\|\\+\\-\\=\\_\\)\\(\\)\\`\\/\\\\\\]])[A-Za-z0-9d$@].{8,}'
             )
         ]),
     });
@@ -53,6 +53,7 @@ export class LoginComponent {
 
     //#region Validators
     protected emailError$ = this.userForm.valueChanges.pipe(
+        debounceTime(1500),
         map(() => {
             if (!this.userForm.controls.email.touched || this.userForm.controls.email.valid) {
                 return '';
@@ -65,6 +66,7 @@ export class LoginComponent {
     );
 
     protected usernameError$ = this.userForm.valueChanges.pipe(
+        debounceTime(1500),
         map(() => {
             if (!this.userForm.controls.username.touched || this.userForm.controls.username.valid) {
                 return '';
@@ -77,12 +79,22 @@ export class LoginComponent {
     );
 
     protected passwordError$ = this.userForm.valueChanges.pipe(
-        // TODO - Implement password error handling to get a single error message
+        debounceTime(1500),
         map(() => {
-            if (!this.userForm.controls.username.touched || this.userForm.controls.username.valid) {
+            const value = this.userForm.controls.password.value;
+            if (!value || !this.userForm.controls.password.touched || this.userForm.controls.password.valid) {
                 return '';
             }
-            return 'error';
+            if (!RegExp('.*[A-Z].*').test(value)) {
+                return 'Password must contain at least one capital letter.';
+            }
+            if (!RegExp('.*[0-9].*').test(value)) {
+                return 'Password must contain at least one number.';
+            }
+            if (!RegExp('.{8,}').test(value)) {
+                return 'Password must be at least 8 characters long.';
+            }
+            return 'Password must contain a special character.';
         })
     );
     //#endregion
